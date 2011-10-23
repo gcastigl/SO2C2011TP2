873329898e1fc7493b16f0f305133076587254ff
juan.orsay <juan.orsay@avature.net>
2011-10-23 02:35:33 -0300

    started working on OPEN

--- include/permission/permission.h
+++ include/permission/permission.h
@@ -10,5 +10,19 @@
 #define W_BIT           0000002	/* rWx protection bit */
 #define X_BIT           0000001	/* rwX protection bit */
 
+#define S_IRWXU 00700 //User RWX
+#define S_IRUSR 00400 //User R
+#define S_IWUSR 00200 //User W
+#define S_IXUSR 00100 //User X
+#define S_IRWXG 00070 //Group RWX
+#define S_IRGRP 00040 //Group R
+#define S_IWGRP 00020 //Group W
+#define S_IXGRP 00010 //Group X
+#define S_IRWXO 00007 //Others RWX
+#define S_IROTH 00004 //Others R
+#define S_IWOTH 00002 //Others W
+#define S_IXOTH 00001 //Others X
+
+
 #endif	/* PERMISSION_H */
 
