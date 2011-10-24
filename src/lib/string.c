f990364ee45ad0b96c4bf1f11176757b4daf1b5b
horacio.gomez <horacio.gomez@avature.net>
2011-10-24 00:47:46 -0300

    added new str functions.

--- src/lib/string.c
+++ src/lib/string.c
@@ -86,3 +86,51 @@ int strContains(char* str, char c) {
 	}
 	return false;
 }
+
+int strspn(const char *string, const char *in) {
+	register const char *s1, *s2;
+
+	for (s1 = string; *s1; s1++) {
+		for (s2 = in; *s2 && *s2 != *s1; s2++)
+			/* EMPTY */;
+		if (*s2 == '\0')
+			break;
+	}
+	return s1 - string;
+}
+
+char *strtok(register char *string, const char *separators) {
+	register char *s1, *s2;
+	static char *savestring = NULL;
+
+	if (string == NULL) {
+		string = savestring;
+		if (string == NULL)
+			return (char *) NULL;
+	}
+
+	s1 = string + strspn(string, separators);
+	if (*s1 == '\0') {
+		savestring = NULL;
+		return (char *) NULL;
+	}
+
+	s2 = strpbrk(s1, separators);
+	if (s2 != NULL)
+		*s2++ = '\0';
+	savestring = s2;
+	return s1;
+}
+
+char *strpbrk(register const char *string, register const char *brk) {
+	register const char *s1;
+
+	while (*string) {
+		for (s1 = brk; *s1 && *s1 != *string; s1++)
+			/* EMPTY */;
+		if (*s1)
+			return (char *) string;
+		string++;
+	}
+	return (char *) NULL;
+}
