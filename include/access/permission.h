151d3d7877cd6183bdc649a0a6743ed1b230c251
horacio.gomez <horacio.gomez@avature.net>
2011-10-31 18:39:14 -0300

    signature doc

--- include/access/permission.h
+++ include/access/permission.h
@@ -10,10 +10,29 @@
 
 #define MASK_STRING_LEN 10+1
 
-PUBLIC void mask_string(int perm, char *string);
+/**
+ * Writes in string the file mask. Must have MASK_STRING_LEN allocated.
+ */
+PUBLIC void mask_string(int mask, char *string);
+
+/**
+ * Returns true if the currentUser is the owner of the user or root.
+ */
 PUBLIC boolean permission_user_isOwner(int uid);
+
+/**
+ * Returns true if the currentUser is the owner of the group or root.
+ */
 PUBLIC boolean permission_group_isOwner(int gid);
+
+/**
+ * Returns true if the currentUser is the owner of the file or root.
+ */
 PUBLIC boolean permission_file_isOwner(fs_node_t node);
+
+/**
+ * Returns true if the currentUser has the desired access or is root.
+ */
 PUBLIC boolean permission_file_hasAccess(fs_node_t node, int access_desired);
 
 #endif	/* PERMISSION_H */
--- src/access/permission.c
+++ src/access/permission.c
@@ -1,34 +1,34 @@
 #include <access/permission.h>
 
-PUBLIC void mask_string(int perm, char *string) {
+PUBLIC void mask_string(int mask, char *string) {
     char type;
-    if ((perm & FS_SOCKET) == FS_SOCKET) {
+    if ((mask & FS_SOCKET) == FS_SOCKET) {
         type = 's';
-    } else if ((perm & FS_SYMLINK) == FS_SYMLINK) {
+    } else if ((mask & FS_SYMLINK) == FS_SYMLINK) {
         type = 'l';
-    } else if ((perm & FS_FILE) == FS_FILE) {
+    } else if ((mask & FS_FILE) == FS_FILE) {
         type = '-';
-    } else if ((perm & FS_BLOCKDEVICE) == FS_BLOCKDEVICE) {
+    } else if ((mask & FS_BLOCKDEVICE) == FS_BLOCKDEVICE) {
         type = 'b';
-    } else if ((perm & FS_DIRECTORY) == FS_DIRECTORY) {
+    } else if ((mask & FS_DIRECTORY) == FS_DIRECTORY) {
         type = 'd';
-    } else if ((perm & FS_CHARDEVICE) == FS_CHARDEVICE) {
+    } else if ((mask & FS_CHARDEVICE) == FS_CHARDEVICE) {
         type = 'c';
-    } else if ((perm & FS_PIPE) == FS_PIPE) {
+    } else if ((mask & FS_PIPE) == FS_PIPE) {
         type = 'p';
     } else {
         type = 'u'; // Desconocido
     }
     string[0] = type;
-    string[1] = ((perm & S_IRUSR) ? 'r' : '-');
-    string[2] = ((perm & S_IWUSR) ? 'w' : '-');
-    string[3] = ((perm & S_IXUSR) ? 'x' : '-');
-    string[4] = ((perm & S_IRGRP) ? 'r' : '-');
-    string[5] = ((perm & S_IWGRP) ? 'w' : '-');
-    string[6] = ((perm & S_IXGRP) ? 'x' : '-');
-    string[7] = ((perm & S_IROTH) ? 'r' : '-');
-    string[8] = ((perm & S_IWOTH) ? 'w' : '-');
-    string[9] = ((perm & S_IXOTH) ? 'x' : '-');
+    string[1] = ((mask & S_IRUSR) ? 'r' : '-');
+    string[2] = ((mask & S_IWUSR) ? 'w' : '-');
+    string[3] = ((mask & S_IXUSR) ? 'x' : '-');
+    string[4] = ((mask & S_IRGRP) ? 'r' : '-');
+    string[5] = ((mask & S_IWGRP) ? 'w' : '-');
+    string[6] = ((mask & S_IXGRP) ? 'x' : '-');
+    string[7] = ((mask & S_IROTH) ? 'r' : '-');
+    string[8] = ((mask & S_IWOTH) ? 'w' : '-');
+    string[9] = ((mask & S_IXOTH) ? 'x' : '-');
     string[10] = '\0';
 }
 
