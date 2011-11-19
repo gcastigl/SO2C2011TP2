a9de49363777d59d3a23381f6ce11ce8b1be6799
gcastigl <gcastigl@gmail.com>
2011-11-19 11:00:11 -0300

    Fixed problem when a reder open the fifo first, it was never woken up. removed commented code.

--- src/lib/fifo.c
+++ src/lib/fifo.c
@@ -8,11 +8,8 @@ PRIVATE fifo_t fifos[MAX_FIFOS];
 PRIVATE int pipe_index(u32int inode);
 
 PRIVATE int signalWriters(fifo_t* f);
-/*
-PRIVATE boolean _checkReadingProcess(int inode, PROCESS** list, int currentPID);
-PRIVATE boolean _checkWritingProcess(int inode, PROCESS** list, int currentPID);
-PRIVATE boolean _checkConditionProcess(int inode, PROCESS** list, int currentPID, int condition);
-*/
+PRIVATE int signalReaders(fifo_t* f);
+PRIVATE int sigalCondition(fifo_t* fifo, int condition);
 
 void fifo_init() {
 	for (int i = 0; i < MAX_FIFOS; ++i) {
@@ -68,16 +65,15 @@ u32int fifo_read(fs_node_t *node, u32int offset, u32int size, u8int *buffer) {
 	//printf("READ: %s - %d bytes / offset: %d", fifo->buff, fifo->offset, size);
 	log(L_DEBUG, "<-- I'm reading the buffer %d bytes from offset %d....\n", size, fifo->offset);
 	memcpy(buffer, fifo->buff + fifo->offset, size);
-	fifo->offset += size;
 	sem_signal(&fifo->lock);
 	sem_wait(&fifo->readers);
+	if (fifo->readers.count == 0) {	// If last reader...
+		fifo->offset += size;
+	}
 	yield();
 	return size;
 }
 
-// wait until no readers or writers
-// access database
-// wake up waiting readers or writers
 u32int fifo_write(fs_node_t *node, u32int offset, u32int size, u8int *buffer) {
 	if (FILE_TYPE(node->mask) != FS_PIPE) {
 		log(L_ERROR, "Trying to write to a file that is not a pipe!");
@@ -101,27 +97,38 @@ u32int fifo_write(fs_node_t *node, u32int offset, u32int size, u8int *buffer) {
 		scheduler_setStatus(scheduler_currentPID(), BLOCKED);
 		yield();
 	}
+	signalReaders(fifo);
 	memcpy(fifo->buff, buffer, size);
 	fifo->lenght = size;
 	log(L_DEBUG, "--> I'm writing to the buffer %d bytes....\n", size);
 	sem_signal(&fifo->lock);
 	while(fifo->offset < size)
-		;
+		; // FIXME: The writer should be set to CLOCKED here...
 	sem_wait(&fifo->writers);
-	fifo->offset = 0;
+	if (fifo->writers.count == 0) {	// If last writer...
+		fifo->offset = 0;
+	}
 	return size;
 }
 
-PRIVATE int signalWriters(fifo_t* f) {
+PRIVATE int signalWriters(fifo_t* fifo) {
+	return sigalCondition(fifo, O_WRONLY);
+}
+
+PRIVATE int signalReaders(fifo_t* fifo) {
+	return sigalCondition(fifo, O_RDONLY);
+}
+
+PRIVATE int sigalCondition(fifo_t* fifo, int condition) {
 	_cli();
 	int signaled = 0;
-	for (int i = 0; i < list_size(&f->waitingQueue); i++) {
-		PROCESS* p = list_get(&f->waitingQueue, i);
+	for (int i = 0; i < list_size(&fifo->waitingQueue); i++) {
+		PROCESS* p = list_get(&fifo->waitingQueue, i);
 		for (int j = 0; j < MAX_FILES_PER_PROCESS; ++j) {
-			if (p->fd_table[j].mask != 0 && (p->fd_table[j].mode&&O_WRONLY) != 0) {
+			if (p->fd_table[j].mask != 0 && (p->fd_table[j].mode&&condition) != 0) {
 				scheduler_setStatus(p->pid, READY);
-				PROCESS* p = list_remove(&f->waitingQueue, i);
-				log(L_DEBUG, "%d has been removed from the queue / size: %d", p->pid, f->waitingQueue.size);
+				PROCESS* p = list_remove(&fifo->waitingQueue, i);
+				log(L_DEBUG, "%d has been removed from the queue / size: %d", p->pid, fifo->waitingQueue.size);
 				signaled++;
 				break;
 			}
@@ -130,32 +137,3 @@ PRIVATE int signalWriters(fifo_t* f) {
 	_sti();
 	return signaled;
 }
-
-/*
-PRIVATE boolean _checkReadingProcess(int inode, PROCESS** list, int currentPID) {
-	return _checkConditionProcess(inode, list, currentPID, O_RDONLY);
-}
-
-PRIVATE boolean _checkWritingProcess(int inode, PROCESS** list, int currentPID) {
-	return _checkConditionProcess(inode, list, currentPID, O_WRONLY);
-}
-
-PRIVATE boolean _checkConditionProcess(int inode, PROCESS** list, int currentPID, int condition) {
-	for (int i = 0; i < MAX_PROCESSES; i++) {
-		PROCESS* p = list[i];
-		// p is not me, is waiting for a fifo with inode "inode"
-		if (p != NULL && p->pid != currentPID &&
-			p->waitingFlags == W_FIFO && p->waitingInfo == inode) {
-			for (int j = 0; j < MAX_FILES_PER_PROCESS; ++j) {
-				if (p->fd_table[j].mask != 0 && (p->fd_table[j].mode&&condition) != 0) {
-					log(L_DEBUG, "inode: %s, %d => %d", p->name, j, p->fd_table[j].inode);
-					return true;
-				}
-			}
-		}
-	}
-	return false;
-}
-
-
-*/
