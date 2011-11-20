181100f055ec7a5e2b2cfb62e4ff9b2e4393e4c7
gcastigl <gcastigl@gmail.com>
2011-11-20 10:43:09 -0300

    made some chagges to the was the stack is stored

--- include/process/process.h
+++ include/process/process.h
@@ -4,20 +4,29 @@
 #include <defs.h>
 #include <lib/kheap.h>
 
-#define DEFAULT_STACK_SIZE 0x400
+#define DEFAULT_STACK_SIZE 0x1000
+
+#define _EIP 	read_eip()
+#define _CS 	read_cs()
+#define _SS 	read_ss()
+#define _DS 	read_ds()
+
+#define INIFL 	0x200
+#define FP_SEG(fptr) 	((unsigned)((unsigned long)(fptr) >> 16))
+#define FP_OFF(fptr)	((unsigned)(fptr))
+
 // I think there is one like this already defined...
-#define MAX_PROCESS_NAME 	16
-#define MAX_PROCESSES		32
-#define MAX_ARG             16
-#define P_RATIO             2
-
-typedef enum {BLOCKED = 0, READY, RUNNING} status_t;
-typedef enum {BACKGROUND = 0, FOREGROUND} groundness_t;
-typedef enum {PNONE = 0, VERY_LOW, LOW, NORMAL, HIGH, VERY_HIGH, SKY_HIGH} priority_t;
-typedef enum {W_FIFO = 0, W_INPUT} block_t;
-
-#define MAX_PRIORITY HIGH
+#define MAX_PROCESS_NAME 		16
+#define MAX_PROCESSES			32
+#define MAX_ARG             	16
+#define P_RATIO             	2
 #define MAX_FILES_PER_PROCESS	10
+
+typedef enum {PNONE = 0, VERY_LOW, LOW, NORMAL, HIGH, VERY_HIGH, SKY_HIGH} priority_t;
+typedef enum {BACKGROUND = 0, FOREGROUND} 	groundness_t;
+typedef enum {BLOCKED = 0, READY, RUNNING} 	status_t;
+typedef enum {W_FIFO = 0, W_INPUT} 			block_t;
+
 
 typedef struct {
 	char name[MAX_NAME_LENGTH];
@@ -38,13 +47,12 @@ typedef struct {
 	char *argv[MAX_ARG];
 	int ESP;
 	int stacksize;
-	int stackstart;
+	int stack;
 	// Process state
 	int pid;
 	int parent;
 	groundness_t groundness;
 	priority_t priority;
-	int sleep;
 	status_t status;
 	int lastCalled;
 	block_t waitingFlags;
