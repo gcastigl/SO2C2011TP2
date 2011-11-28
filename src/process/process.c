3bf7c75e1f038b20237999b901fc2a5545c30f05
juan.orsay <juan.orsay@avature.net>
2011-11-28 18:33:01 -0300

    changed function name and corrected some few things

--- src/process/process.c
+++ src/process/process.c
@@ -104,12 +104,20 @@ PRIVATE void _expandStack() {
     _sti();
 }
 
-PUBLIC void process_updateStack() {
+PUBLIC void process_checkStack() {
     PROCESS *proc = scheduler_getCurrentProcess();
     if (proc == NULL)
         return;
-    if ((proc->stack - proc->ESP) > (proc->stacksize / 2)) {
-        log(L_INFO, "Expanding stack(0x%x) for %s", proc->stack, proc->name);
+    if (proc->pid > MAX_TTYs)
+        log(L_INFO, "Process %s: ESP: 0x%x stackStart: 0x%x", proc->name, proc->ESP, proc->stack);
+/*    if (proc->ESP < proc->stack + 0x500) {
+        registers_t regs;
+        regs.esp = proc->ESP;
+        panic("asdasdasd", 1, true);
+    }
+*/    
+    if (proc->ESP < proc->stack + 0x500) {
+        log(L_INFO, "Expanding stack(0x%x @ 0x%x) for %s", proc->stack, proc->name);
         _expandStack();
         log(L_INFO, "EXPANDED stack(0x%x) for %s", proc->stack, proc->name);
     }
