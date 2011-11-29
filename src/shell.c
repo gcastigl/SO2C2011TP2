7ce21b85d9b6b402930066114d2c00a8cf0f2329
gcastigl <gcastigl@gmail.com>
2011-11-29 01:51:56 -0300

    bufferOffset was renamed to screenOffset

--- src/shell.c
+++ src/shell.c
@@ -168,7 +168,7 @@ char** getArguments(char* buffer, int* argc, int *background) {
 }
 
 void cleanBuffer(TTY *tty) {
-    tty->bufferOffset = 0;
+    tty->screenOffset = 0;
     tty->buffer[0] = '\0';
 }
 
